a8b6a918ffa989e5089b0a5cdfff6502cf79590a
Francisco Muñoz <femunoz@dim.uchile.cl>
2021-05-09 16:48:03 -0400

    Implementado y terminado la base de la clase TypeString con sus tests.

--- src/main/java/cl/uchile/dcc/scrabble/model/types/TypeString.java
+++ src/main/java/cl/uchile/dcc/scrabble/model/types/TypeString.java
@@ -1,4 +1,45 @@
 package cl.uchile.dcc.scrabble.model.types;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class TypeString extends AbstractType {
+    private String value;
+
+    /**
+     * Constructor for the TypeString.
+     * @param value A String as a value.
+     */
+    public TypeString(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TypeString)) return false;
+        TypeString that = (TypeString) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+
+    /**
+     * Transforms the current type to a TypeString.
+     *
+     * @return TypeString with a value equivalent to the current type.
+     */
+    @Override
+    public TypeString toTypeString() {
+        return new TypeString(this.value);
+    }
 }
---
+++ src/test/java/cl/uchile/dcc/scrabble/model/types/TypeStringTest.java
@@ -0,0 +1,62 @@
+package cl.uchile.dcc.scrabble.model.types;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TypeStringTest {
+    private int seed;
+    private TypeString typeString;
+    private TypeString otherTypeString;
+    private String aString;
+    private String otherString;
+
+    @BeforeEach
+    void setUp() {
+        // Initialize a random seed and a random rng
+        seed = new Random().nextInt(1000);
+        Random rng = new Random(seed);
+        // Initialize a random string size
+        int strSize = rng.nextInt(20);
+        // Initialize random strings
+        aString = RandomStringUtils.random(strSize, 0, Character.MAX_CODE_POINT, true, true, null, rng);
+        do {
+            if (strSize == 0) { strSize = 1; }
+            otherString = RandomStringUtils.random(strSize, 0, Character.MAX_CODE_POINT, true, true, null, rng);
+        } while (otherString.equals(aString));
+        // Makes instances of TypeString
+        typeString = new TypeString(aString);
+        otherTypeString = new TypeString(otherString);
+    }
+
+    @RepeatedTest(20)
+    void testEquals() {
+        TypeString similarTypeString = new TypeString(aString);
+        assertNotEquals(otherTypeString, typeString, "Two different instances are equals when it is not.");
+        assertEquals(similarTypeString, typeString, "Two different instances are not equals when it is.");
+    }
+
+    @RepeatedTest(20)
+    void testHashCode() {
+        TypeString similarTypeString = new TypeString(aString);
+        assertNotEquals(otherTypeString.hashCode(), typeString.hashCode(), "Hash Code does not work.");
+        assertEquals(similarTypeString.hashCode(), typeString.hashCode(), "Hash Code does not work.");
+    }
+
+    @RepeatedTest(20)
+    void testToString() {
+        assertEquals(aString, typeString.toString(), "Method toString does not works.");
+        assertNotEquals(otherString, typeString.toString(), "Method toString does not works.");
+    }
+
+    @RepeatedTest(value = 20)
+    void toTypeString() {
+        TypeString similarTypeString = new TypeString(aString);
+        assertEquals(similarTypeString, typeString.toTypeString(), "Method toTypeString does not work.");
+    }
+}
