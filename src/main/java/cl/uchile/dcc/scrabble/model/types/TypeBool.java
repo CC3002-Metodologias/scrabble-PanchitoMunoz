bc307dee4ed3bef231d396ba5eb2c0a66d22bb50
Francisco Muñoz <femunoz@dim.uchile.cl>
2021-05-09 16:47:30 -0400

    Implementado y terminado la base de la clase TypeBool con sus tests

--- src/main/java/cl/uchile/dcc/scrabble/model/types/TypeBool.java
+++ src/main/java/cl/uchile/dcc/scrabble/model/types/TypeBool.java
@@ -1,17 +1,56 @@
 package cl.uchile.dcc.scrabble.model.types;
 
+import java.util.Objects;
+
 /**
  * A class for the boolean type.
  * @author Francisco Muñoz Guajardo
  */
 public class TypeBool extends AbstractType {
-    private Boolean value;
+    private boolean value;
 
     /**
      * Constructor for the BoolType.
      * @param value A boolean as a value.
      */
-    public TypeBool(Boolean value) {
+    public TypeBool(boolean value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TypeBool)) return false;
+        TypeBool typeBool = (TypeBool) o;
+        return Objects.equals(value, typeBool.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return Boolean.toString(this.value);
+    }
+
+    /**
+     * Transforms the current type to a TypeString.
+     *
+     * @return TypeString with a value equivalent to the current type.
+     */
+    @Override
+    public TypeString toTypeString() {
+        return new TypeString(this.toString());
+    }
+
+    /**
+     * Transforms the current type to a TypeBool.
+     *
+     * @return TypeBool with a value equivalent to the current type.
+     */
+    public TypeBool toTypeBool() {
+        return new TypeBool(this.value);
     }
 }
--- src/test/java/cl/uchile/dcc/scrabble/model/types/TypeBoolTest.java
+++ src/test/java/cl/uchile/dcc/scrabble/model/types/TypeBoolTest.java
@@ -1,8 +1,58 @@
 package cl.uchile.dcc.scrabble.model.types;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class TypeBoolTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+class TypeBoolTest {
+    private TypeBool trueTypeBool;
+    private TypeBool falseTypeBool;
+    private final Boolean trueBoolean = true;
+    private final Boolean falseBoolean = false;
+
+    @BeforeEach
+    void setUp() {
+        trueTypeBool = new TypeBool(trueBoolean);
+        falseTypeBool = new TypeBool(falseBoolean);
+    }
+
+    @Test
+    void testEquals() {
+        TypeBool otherTrueTypeBool = new TypeBool(trueBoolean);
+        TypeBool otherFalseTypeBool = new TypeBool(falseBoolean);
+        assertEquals(otherTrueTypeBool, trueTypeBool, "Same object are not equals.");
+        assertEquals(otherFalseTypeBool, falseTypeBool, "Same objects are not equals.");
+    }
+
+    @Test
+    void testHashCode() {
+        TypeBool otherTrueTypeBool = new TypeBool(trueBoolean);
+        TypeBool otherFalseTypeBool = new TypeBool(falseBoolean);
+        assertEquals(otherTrueTypeBool.hashCode(), trueTypeBool.hashCode(), "Hash Codes are not equals.");
+        assertEquals(otherFalseTypeBool.hashCode(), falseTypeBool.hashCode(), "Hash Codes are not equals.");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("true", trueTypeBool.toString(), "toString method does not work with true.");
+        assertEquals("false", falseTypeBool.toString(), "toString method does not work with false.");
+    }
+
+    @Test
+    void toTypeString() {
+        TypeString typeString = new TypeString(trueTypeBool.toString());
+        assertEquals(typeString, trueTypeBool.toTypeString(), "The method toTypeString does not works.");
+    }
+
+    @Test
+    void toTypeBool() {
+        TypeBool otherTrueTypeBool = new TypeBool(trueBoolean);
+        TypeBool otherFalseTypeBool = new TypeBool(falseBoolean);
+        assertEquals(otherTrueTypeBool, trueTypeBool.toTypeBool(), "The method toTypeBool does not works with true.");
+        assertNotEquals(otherFalseTypeBool, trueTypeBool.toTypeBool(), "The method toTypeBool does not works with true.");
+        assertEquals(otherFalseTypeBool, falseTypeBool.toTypeBool(), "The method toTypeBool does not works with true.");
+        assertNotEquals(otherTrueTypeBool, falseTypeBool.toTypeBool(), "The method toTypeBool does not works with true.");
+    }
 }
