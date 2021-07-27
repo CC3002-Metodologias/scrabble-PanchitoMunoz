81ea3e51692c8c59e3525b6aab247c443bc101f5
Francisco Muñoz <femunoz@dim.uchile.cl>
2021-07-27 19:02:46 -0400

    feat: agregadas clases de operadores relacionales

---
+++ src/main/java/cl/uchile/dcc/scrabble/model/hidden_layer/hidden_operators/abstract_classes/AbstractHiddenRelationalOperator.java
@@ -0,0 +1,27 @@
+package cl.uchile.dcc.scrabble.model.hidden_layer.hidden_operators.abstract_classes;
+
+import cl.uchile.dcc.scrabble.model.hidden_layer.HiddenASTComponent;
+
+/**
+ * todo: doc
+ *
+ * @author Francisco Muñoz Guajardo
+ * @create 2021/07/26 0:34
+ */
+public abstract class AbstractHiddenRelationalOperator extends AbstractHiddenBinaryOperator {
+
+    /**
+     * Default constructor. It can receive an {@code HiddenOperator} or a {@code HType}.
+     *
+     * @param leftValue      left value, it can be an {@code HiddenOperator} or a {@code HType}.
+     * @param rightValue     right value, it can be an {@code HiddenOperator} or a {@code HType}.
+     * @param operatorName   the operator's name
+     * @param operatorSymbol the operator's symbol
+     */
+    public AbstractHiddenRelationalOperator(
+        HiddenASTComponent leftValue,
+        HiddenASTComponent rightValue,
+        String operatorName, String operatorSymbol) {
+        super(leftValue, rightValue, operatorName, operatorSymbol);
+    }
+}
---
+++ src/main/java/cl/uchile/dcc/scrabble/model/hidden_layer/hidden_operators/hidden_relational_operator/HiddenLowerThan.java
@@ -0,0 +1,42 @@
+package cl.uchile.dcc.scrabble.model.hidden_layer.hidden_operators.hidden_relational_operator;
+
+import cl.uchile.dcc.scrabble.model.hidden_layer.HiddenASTComponent;
+import cl.uchile.dcc.scrabble.model.hidden_layer.hidden_operators.abstract_classes.AbstractHiddenRelationalOperator;
+import cl.uchile.dcc.scrabble.model.hidden_layer.hidden_types.HType;
+
+/**
+ * todo: doc
+ *
+ * @author Francisco Muñoz Guajardo
+ * @create 2021/07/27 18:58
+ */
+public class HiddenLowerThan extends AbstractHiddenRelationalOperator {
+
+    /**
+     * Default constructor. It can receive an {@code HiddenOperator} or a {@code HType}.
+     *
+     * @param leftValue      left value, it can be an {@code HiddenOperator} or a {@code HType}.
+     * @param rightValue     right value, it can be an {@code HiddenOperator} or a {@code HType}.
+     * @param operatorName   the operator's name
+     * @param operatorSymbol the operator's symbol
+     */
+    public HiddenLowerThan(
+        HiddenASTComponent leftValue,
+        HiddenASTComponent rightValue,
+        String operatorName, String operatorSymbol) {
+        super(leftValue, rightValue, operatorName, operatorSymbol);
+    }
+
+    /**
+     * Compute the operation between 2 {@code HType} and returns its operation. To use template
+     * pattern in {@code calculate}.
+     *
+     * @param value1 the value at the left
+     * @param value2 the value at the right
+     * @return the value computed
+     */
+    @Override
+    protected HType mainOperation(HType value1, HType value2) {
+        return null;
+    }
+}
